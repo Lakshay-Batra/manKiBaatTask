8243e31b25855d34d8d10f59c9f6f231e75a1314
Demo-Lakshay <50356418+Lakshay-Batra@users.noreply.github.com>
2020-06-11 16:46:55 +0530

    Update to words rather than characters

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -2,42 +2,79 @@ import React, { useState } from "react";
 import Fab from "@material-ui/core/Fab";
 import AddIcon from "@material-ui/icons/Add";
 import Zoom from "@material-ui/core/Zoom";
-import { red } from "@material-ui/core/colors";
 
 function CreateArea() {
 
   const [inputString, setInputString] = useState("");
-  const [isExpanded, setExpanded] = useState(false);
-  const [inputStringLength, setInputStringLength] = useState(0);
+  const [wordCount, setWordCount] = useState(0);
+  const [upperLimit, setUpperLimit] = useState("");
+  const [upperLimitIsEntered, setUpperLimitEntered] = useState(0);
+  const [maxStringLength, setMaxStringLength] = useState(null);
 
   function handleChange(event) {
-    const { value } = event.target;
-    const truncateString = value.slice(0, 180);
-    const stringLength = truncateString.length;
+    const { name, value } = event.target;
+    if (name === "content") {
+      
+      var str = value;
 
-    setInputString(value);
-    setInputStringLength(stringLength);
+      str = str.replace(/[ ]{2,}/gi, " ");
+      str = str.replace(/\n /, "\n");
+
+
+      if (str.split(" ").length === parseInt(upperLimit) + 1) {
+        if (str.match(/(\s$)/gi)) {
+          setMaxStringLength(value.length);
+        } else {
+          setMaxStringLength(null)
+        }
+      }
+
+      str = str.replace(/(^\s*)|(\s*$)/gi, "");
+
+      setInputString(value);
+      setWordCount(str.split(' ').length);
+      if(value === "") {
+        setWordCount(0)
+      }
+    } else {
+      setUpperLimit(value);
+    }
   }
 
-
-  function expand() {
-    setExpanded(true);
+  function handleSubmit(event) {
+    if (upperLimit !== "") {
+      setUpperLimitEntered(1);
+    }
+    event.preventDefault();
   }
+
+  const handleKeyDown =  (evt) => {
+    if(evt.key === "Backspace") {
+      setMaxStringLength(null);
+    }
+    if(maxStringLength !== null) {
+      evt.preventDefault();
+    }
+  } 
 
   const alertStyle = {
-    color: !(180-inputStringLength) ? "red" : "rgb(119, 117, 117)"
+    color: !(upperLimit - wordCount) ? "red" : "rgb(119, 117, 117)"
   }
 
   return (
     <div>
-      <div className="create-note">
-        <textarea maxLength="180" onClick={expand} onChange={handleChange} name="content" placeholder="Take a note..." rows={isExpanded ? "4" : "1"} value={inputString} />
-        <Zoom in={isExpanded}>
+      {!upperLimitIsEntered ?
+        <form onSubmit={handleSubmit} className="create-note">
+          <input type="number" onChange={handleChange} name="length" placeholder="Enter Upper Limit of Words" value={upperLimit} />
+          <Fab type="submit"><AddIcon /></Fab>
+        </form>
+        :
+        <div className="create-note">
+          <textarea onKeyDown={handleKeyDown} maxLength={maxStringLength} onChange={handleChange} name="content" placeholder="Take a note..." rows="4" value={inputString} />
           <Fab><AddIcon /></Fab>
-        </Zoom>
-
-        {isExpanded && <p className="string-counter" style={alertStyle}>Limit: {inputStringLength}/180 and {180-inputStringLength} left</p>}
-      </div>
+          <p className="string-counter" style={alertStyle}>{wordCount}/{upperLimit} and {upperLimit - wordCount} remaining</p>
+        </div>
+      }
     </div>
   );
 }
